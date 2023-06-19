89bb5306b953e7078fb01a1409c525d876edb84d
mpreter <dshabelyan@mail.ru>
2023-06-19 12:24:42 +0300

    Create tests

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,65 +1,40 @@
 import React from "react";
-import { render, within } from "@testing-library/react";
+import { act, render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
-import graphs from "../src/mocks/graphs";
 import App from "../src/App";
+import graphs from "../src/mocks/graphs";
 
-function setup(jsx: JSX.Element) {
-  return {
-    user: userEvent.setup(),
-    ...render(jsx),
-  };
-}
-
-test("App renders", () => {
+beforeEach(async () => {
   render(<App />);
+  await screen.findAllByRole("option");
 });
 
-test("There is a dropdown with all the available graphs", () => {
-  const { getByRole } = setup(<App />);
-  const options = within(getByRole("combobox")).getAllByRole("option");
+test("App renders", async () => {
+  await act(async () => {
+    render(<App />);
+  });
+});
 
-  expect(options.length).toBe(graphs.length);
+test("There is a dropdown with all the available graphs", async () => {
+  const graphList = await screen.findByRole("combobox");
+  const graphListOptions = await screen.findAllByRole("option");
+
+  expect(graphList).not.toBeUndefined();
+  expect(graphListOptions.length).toBe(graphs.length);
 });
 
 test("Graph selected by dropdown is rendered", async () => {
-  const selectedGraph = 2;
-  const { getByRole, findByText, user } = setup(<App />);
-
-  user.selectOptions(getByRole("combobox"), `${selectedGraph}`);
-
-  for (let node of graphs[selectedGraph].nodes) {
-    await findByText(node.name);
-  }
-});
-
-test("Nodes in simple graph are organized into columns", async () => {
   const selectedGraph = 1;
-  const { getByRole, findByText, user } = setup(<App />);
 
-  const correctColumns = [["start"], ["foo", "bar"], ["end1", "end2"]];
+  act(async () => {
+    const selectElement = await screen.findByRole("combobox");
+    await userEvent.selectOptions(selectElement, `${selectedGraph}`);
+  });
 
-  user.selectOptions(getByRole("combobox"), `${selectedGraph}`);
-
-  for (let idx = 0; idx < correctColumns.length; idx++) {
-    const col = correctColumns[idx];
-    const otherCols = correctColumns.flatMap((c, i) => (i === idx ? [] : c));
-    // find the common parent which doesn't hold any other columns
-    let parent = await findByText(col[0]);
-    while (parent) {
-      parent = parent.parentElement as HTMLElement;
-      const hasEveryCorrect = col.every((n) => !!within(parent).queryByText(n));
-      const doesntHaveAnyOther = otherCols.every(
-        (n) => !within(parent).queryByText(n)
-      );
-      if (hasEveryCorrect) {
-        expect(doesntHaveAnyOther).toBeTruthy();
-        break;
-      }
+  await waitFor(() => {
+    for (let node of graphs[selectedGraph].nodes) {
+      expect(screen.findByTestId(node.name)).not.toBeUndefined();
     }
-
-    // make sure we found a parent that contained the current column
-    expect(parent).not.toBeNull();
-  }
+  });
 });
